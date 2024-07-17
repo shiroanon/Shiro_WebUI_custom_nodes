b3a423707da4bb9063abc9baa358ac9264acf759
ShashwatDubey99 <dubeyshashwat9898@gmai.com>
2024-07-17 20:52:04 +0530

    g

--- __init__.py
+++ __init__.py
@@ -1,8 +1,21 @@
 from .out_request import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import os
 import server
+import glob
+def list_image_files(directory):
+    # List of common image file extensions
+    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff', '*.tif', '*.webp']
+    
+    # Initialize an empty list to store image file paths
+    image_files = []
+
+    # Loop through each extension and use glob to find matching files
+    for extension in image_extensions:
+        image_files.extend(glob.glob(os.path.join(directory, extension)))
+    
+    return image_files
 from aiohttp import web
 @server.PromptServer.instance.routes.get("/dungeon")
 def deungeon_entrance(request):
-    return web.json_response("hello")
+    return web.json_response(list_image_files("outputs"))
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
