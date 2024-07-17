6a86d0b7e08d855c78374b22e59993f2f5cb7e07
ShashwatDubey99 <dubeyshashwat9898@gmai.com>
2024-07-17 20:47:15 +0530

    jkjk

--- __init__.py
+++ __init__.py
@@ -1,3 +1,9 @@
+from .out_request import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import os
+import server
+from aiohttp import web
+@server.PromptServer.instance.routes.get("/dungeon")
+def deungeon_entrance(request):
+    return web.json_response("hello"))
 
-NODE_CLASS_MAPPINGS = {}
-NODE_DISPLAY_NAME_MAPPINGS = {}
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
--- out_request.py
+++ out_request.py
@@ -1,26 +1,2 @@
-import os
-import server
-from aiohttp import web
-
-import glob
-current_dir = os.path.dirname(os.path.abspath(__file__))
-@server.PromptServer.instance.routes.get("/dungeon")
-def deungeon_entrance(request):
-    return web.json_response("hello"))
-
-outputs_dir = os.path.join(current_dir, '..', 'outputs')
-
-outputs_dir = os.path.normpath(outputs_dir)
-def list_image_files(directory):
-   
-    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff', '*.tif', '*.webp']
-    
-    image_files = []
-
-    for extension in image_extensions:
-        image_files.extend(glob.glob(os.path.join(directory, extension)))
-    
-    return image_files
-
-
-__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
+NODE_CLASS_MAPPINGS = {}
+NODE_DISPLAY_NAME_MAPPINGS = {}
