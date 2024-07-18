3637c4b50b7095704875b5cb210a451307d9ae76
ShashwatDubey99 <dubeyshashwat9898@gmai.com>
2024-07-18 14:58:08 +0530

    final

--- __init__.py
+++ __init__.py
@@ -3,18 +3,23 @@ import os
 import server
 import glob
 def list_image_files(directory):
-  
+    # List of common image file extensions
     image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff', '*.tif', '*.webp']
     
- 
+    # Initialize an empty list to store image file paths
     image_files = []
 
     # Loop through each extension and use glob to find matching files
     for extension in image_extensions:
-        # Extend the list with just the file names
-        image_files.extend([os.path.basename(file) for file in glob.glob(os.path.join(directory, extension))])
+        image_files.extend(glob.glob(os.path.join(directory, extension)))
     
-    return image_files
+    # Sort files by modification time, newest first
+    image_files.sort(key=lambda x: os.path.getmtime(x), reverse=True)
+    
+    # Extract just the file names
+    image_file_names = [os.path.basename(file) for file in image_files]
+
+    return image_file_names
 from aiohttp import web
 @server.PromptServer.instance.routes.get("/dungeon")
 def deungeon_entrance(request):
