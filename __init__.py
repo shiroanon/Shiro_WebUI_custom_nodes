066fed547fed3205f9e678f9a8d290f65faefb6c
ShashwatDubey99 <dubeyshashwat9898@gmai.com>
2024-07-29 20:58:01 +0530

    hell...

--- __init__.py
+++ __init__.py
@@ -2,6 +2,14 @@ from .out_request import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import os
 import server
 import glob
+def delete_image_file(directory, filename):
+    image_extensions = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.tif', '.webp']
+    for ext in image_extensions:
+        file_path = os.path.join(directory, filename + ext)
+        if os.path.exists(file_path):
+            os.remove(file_path)
+            return True
+    return False
 def list_image_files(directory):
     # List of common image file extensions
     image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.gif', '*.bmp', '*.tiff', '*.tif', '*.webp']
@@ -21,7 +29,16 @@ def list_image_files(directory):
 
     return image_file_names
 from aiohttp import web
-@server.PromptServer.instance.routes.get("/dungeon")
-def deungeon_entrance(request):
+@server.PromptServer.instance.routes.get("/outputs")
+async def get_outputs_img_name(request):
     return web.json_response(list_image_files("outputs"))
+@server.router.delete("/outputs/{filename}")
+async def delete_output_img_name(request):
+    filename = request.match_info['filename']
+    if delete_image_file("outputs", filename):
+        return web.json_response({'status': 'success', 'message': f'File {filename} deleted successfully.'})
+    else:
+        return web.json_response({'status': 'error', 'message': f'File {filename} not found.'}, status=404)
+
+
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
